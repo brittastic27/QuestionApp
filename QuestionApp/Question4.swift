76d34764c20f8b2b4fee4b68c74e6338b4217225
Brittney <brittneybarnwell12@gmail.com>
2024-06-24 18:01:33 -0400

    Add new SwiftUIs

---
+++ QuestionApp/Question4.swift
@@ -0,0 +1,18 @@
+//
+//  Question4.swift
+//  QuestionApp
+//
+//  Created by Brittney Barnwell on 6/24/24.
+//
+
+import SwiftUI
+
+struct Question4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Question4()
+}
---
+++ QuestionApp/Question5.swift
@@ -0,0 +1,18 @@
+//
+//  Question5.swift
+//  QuestionApp
+//
+//  Created by Brittney Barnwell on 6/24/24.
+//
+
+import SwiftUI
+
+struct Question5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Question5()
+}
