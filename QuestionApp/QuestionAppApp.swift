c414dae12b6c8bb3967c7b7b178ec4bc8bf4bd8b
Brittney <brittneybarnwell12@gmail.com>
2024-06-24 11:53:05 -0400

    Initial Commit

---
+++ QuestionApp/QuestionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionAppApp.swift
+//  QuestionApp
+//
+//  Created by Brittney Barnwell on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
